051b7ba9d88afedeeef2d9e6fa188bb0f1cab88e
Josua Mayer <privacy@not.given>
2015-09-13 21:43:05 +0200

    Just print a notice about imx6-config when a user logs in, instead of telling him about potentially uncofnigured systems.
    I simply expect systems to come properly configured in a usable manner.

--- Build-Files/imx6-config.sh
+++ Build-Files/imx6-config.sh
@@ -3,16 +3,11 @@
 #
 # See LICENSE file for copyright and license details
 
-# Should be installed to /etc/profile.d/imx6-config.sh to force imx6-config 
-# to run at initial login
+# Should be installed to /etc/profile.d/imx6-config.sh to
+# inform users about the presence of this program
 
 # You may also want to set automatic login in /etc/inittab on tty1 by adding a 
 # line such as:
 # 1:2345:respawn:/bin/login -f root tty1 </dev/tty1 >/dev/tty1 2>&1 # IMX6CFG_TO_DISABLE
 
-if [ $(id -u) -ne 0 ]; then
-  printf "\nNOTICE: the software on this Imx6 has not been fully configured. Please run 'sudo imx6-config'\n\n"
-else
-  imx6-config
-  exec login -f root
-fi
+echo ${color} "Welcome to Linux on i.MX6. This System comes preinstalled with \"imx6-config\", a user-friendly tool to manipulate system settings."
